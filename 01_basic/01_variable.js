046ec136c948e57c7363e3725d71214402aa6cd4
Denish Chotaliya <128218250+DenishChotaliya@users.noreply.github.com>
2023-11-20 10:24:58 +0000

    datatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,23 @@
+const accountId = 15963
+let accountPassword = "59482"
+var accountGmail = "dk@gmail.com"
+
+accountCity = "Surat"
+
+//accountId = 123 // not allowed it is a const varible 
+
+accountPassword = "123"
+
+
+accountGmail = "123@gmail.com"
+
+accountCity = "Sapur"
+
+/* 
+Prefer not to use var
+becaus of issue in block scope and finctional scope
+*/
+
+
+console.log(accountId);
+console.table([accountPassword,accountGmail,accountCity,accountId])
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version
+
+// alert(4+5) // we are using nodejs, not browser
+
+//console.log(5 + 5); console.log("Denish"); // code readability should be high
+
+console.log(typeof "Denish");
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+// number => 2 tp power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value (one type of object)(ex: console.log(typeof null)  o.p. =>object  ;)
+// undefined => 
+// symbol => unique
+
