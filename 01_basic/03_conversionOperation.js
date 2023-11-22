62a5a88680eb390b65aacb792f6500f1fbdf26ab
Denish Chotaliya <128218250+DenishChotaliya@users.noreply.github.com>
2023-11-22 09:53:47 +0000

    Conversion Operation

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "22"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber );
+
+
+// Notes start //
+// "33 " => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// Notes end //
+
+let isLoggendIn = ""
+
+let booleanIsLoggendIn = Boolean(isLoggendIn)
+
+console.log(booleanIsLoggendIn);
+
+// Notes start for Boolean  //
+// 1 => true ;  0 => false
+// "" => false
+// "denish" => true
+// Notes end //
+
+let number = 33
+
+let stringNumber = String(number)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
