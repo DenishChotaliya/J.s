53b179021a74adf03d73b8693b49e0b224952d83
Denish Chotaliya <128218250+DenishChotaliya@users.noreply.github.com>
2023-11-27 09:38:07 +0000

    conversoinh

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "22"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber );
+// console.log(typeof valueInNumber );
 
 
 // Notes start //
@@ -17,7 +17,7 @@ let isLoggendIn = ""
 
 let booleanIsLoggendIn = Boolean(isLoggendIn)
 
-console.log(booleanIsLoggendIn);
+// console.log(booleanIsLoggendIn);
 
 // Notes start for Boolean  //
 // 1 => true ;  0 => false
@@ -29,5 +29,60 @@ let number = 33
 
 let stringNumber = String(number)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// --------------------------- Operations --------------------------
+
+let value = 6
+
+let nagvalue = -value
+
+// console.log(nagvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);// 2^2
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Denish"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 3); // 13
+// console.log(2 + "4"); // 24
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+
+// console.log(true); // true
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+let a=2
+// console.log(++a);//3
+//  console.log(a++);//2
+
+ let num1, num2, num3
+
+ num1 = num2 = num3 = 2 + 2
+ 
+ let gamecounter = 100
+ gamecounter++;
+ ++gamecounter;
+ console.log(gamecounter);
+
+//  let name = 10
+
+//  name++; // 10 ++ 11
+
+//  ++name; //11
+//  name--; //
+//  console.log(name) 
+
+ 
